873ab4eccbae442d9395f3333ca6703ea45afc02
icxj1 <idchesher@gmail.com>
2018-01-22 14:12:36 -0500

    remove configstore & gnss

--- m7-common.mk
+++ m7-common.mk
@@ -94,8 +94,6 @@ WITH_LINEAGE_CHARGER := false
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    libgps.utils \
     gps.msm8960
 
 PRODUCT_COPY_FILES += \
@@ -135,7 +133,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.configstore@1.0-service \
     copybit.msm8960 \
     gralloc.msm8960 \
     hwcomposer.msm8960 \
