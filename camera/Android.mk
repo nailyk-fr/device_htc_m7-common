ebdc4e16a5bb2dc17f16b9ecf8afb4ca640ff2e0
icxj1 <idchesher@gmail.com>
2018-01-29 18:26:37 -0500

    clean up

--- camera/Android.mk
+++ camera/Android.mk
@@ -6,7 +6,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SRC_FILES := \
-     sensor/SensorManager.cpp  \
+     sensor/SensorManager.cpp \
      ui/GraphicBuffer.cpp \
      ui/GraphicBufferAllocator.cpp \
      ui/GraphicBufferMapper.cpp
